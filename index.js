69cd2e142b4199d81f68246733f7a6ef713a171b
Taha <taha.elmagbri2014@gmail.com>
2022-03-26 03:19:22 -0400

    commit tree 4e1a35bf043299dcc8912fb27ea2d69071f49380

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bowl`);
+    }
+
+    return gifts
+}
+
+wrapGifts(gifts); */
+
+const names = ["Charlie", "Samip", "Ali"];
+
+function writeCards(names, event) {
+    
+    const message = [];
+
+    for (let i = 0; i < names.length; i++) {
+        
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return message;
+}
+
+console.log(writeCards(names, "birthday"));
+
+function countDown (n) {
+    while (n > -1) {
+        console.log(n);
+        n--;
+    }
+}
